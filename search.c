87a73bb50285932996c6a05763f5f8f17bd40ff5
WillStankus <53054354+WillStankus@users.noreply.github.com>
2020-04-24 20:58:43 -0500

    Added comments for points

--- search.c
+++ search.c
@@ -13,7 +13,7 @@ void PrintCard(int offset){
 	FILE *cards_file;
 	cards_file = fopen("cards.bin","rb");
 
-	fseek(cards_file,offset,SEEK_CUR);
+	fseek(cards_file,offset,SEEK_CUR);		//Seek to offset of the card
 	
 	//READ CARD INFO FROM FILE
 	int length = 0;
@@ -22,31 +22,31 @@ void PrintCard(int offset){
 	enum Rarity rarity;
 
 	char *rare_names[] = {"Common","Uncommon","Rare","Mythic"};
-
-	fread(&id,sizeof(int),1,cards_file);
-	fread(&length,sizeof(int),1,cards_file);
+	
+	fread(&id,sizeof(int),1,cards_file);		//Read the id
+	fread(&length,sizeof(int),1,cards_file);	//Read the length of name
 	char name[length];
-	fread(&name,length,1,cards_file);
+	fread(&name,length,1,cards_file);		//Read name
 	
-	fread(&length,sizeof(int),1,cards_file);
+	fread(&length,sizeof(int),1,cards_file);	//Read the length of cost
 	char cost[length];
-	fread(&cost,length,1,cards_file);
+	fread(&cost,length,1,cards_file);		//Read cost
 
-	fread(&converted_cost,sizeof(int),1,cards_file);
+	fread(&converted_cost,sizeof(int),1,cards_file);	//Read converted cost
 	
-	fread(&length,sizeof(int),1,cards_file);
+	fread(&length,sizeof(int),1,cards_file);	//Read length of type
 	char type[length];
-	fread(&type,length,1,cards_file);
+	fread(&type,length,1,cards_file);		//Read type
 
-	fread(&length,sizeof(int),1,cards_file);
+	fread(&length,sizeof(int),1,cards_file);	//Read length of text
 	char text[length];
-	fread(&text,length,1,cards_file);
+	fread(&text,length,1,cards_file);		//Read text
 
-	fread(&length,sizeof(int),1,cards_file);
+	fread(&length,sizeof(int),1,cards_file);	//Read length of stats
 	char stats[length];
-	fread(&stats,length,1,cards_file);
+	fread(&stats,length,1,cards_file);		//Read stats
 
-	fread(&rarity,sizeof(int),1,cards_file);
+	fread(&rarity,sizeof(int),1,cards_file);	//Read rarity
 
 	printf("\n\n%-30s%20s\n%-40s%10s\n%s\n%s\n%s\n%50d\n\n\n", name, cost,type, rare_names[rarity],"--------------------------------------------------",text,"--------------------------------------------------",converted_cost);
 }
@@ -57,14 +57,14 @@ int BinarySearch(char* name, entry* cards, int low, int high){
         int mid = low + (high - low) / 2;
 	char* match = cards[mid].name;
 
-        if (strcmp(name,match) == 0) {	
+        if (strcmp(name,match) == 0) {		//If name matches return
 		return mid;
 	}
             
-        if (strcmp(name,match) < 0) {
+        if (strcmp(name,match) < 0) {		//If name is lower return binary search with the lower half
 		return BinarySearch(name, cards, low, mid - 1); 
 	}
- 	if (strcmp(name,match) > 0) {
+ 	if (strcmp(name,match) > 0) {		//If name is higher return binary search with the upper half
 		 return BinarySearch(name, cards, mid + 1, high); 
 	}
 
@@ -86,16 +86,16 @@ int main(int argc, char* argv[]) {
 	for(int i = 0; i < 1415; i++) {    		
 		int length = 0;
 		int offset = 0;
-		fread(&length,sizeof(int),1,index_file);
+		fread(&length,sizeof(int),1,index_file);	//Read length of name
 		char named[length];
 		
-		fread(named,length,1,index_file);
-		fread(&offset,sizeof(int),1,index_file);
+		fread(named,length,1,index_file);		//Read name
+		fread(&offset,sizeof(int),1,index_file);	//Read offset
 	
 		entry some_card;
 		cards[i] = some_card;
-		strncpy(some_card.name, named, length);
-		some_card.offset = offset;
+		strncpy(some_card.name, named, length);		//String copy name into card name
+		some_card.offset = offset;			//Connect card offset and offset
 	}
 
 	char input[40];
